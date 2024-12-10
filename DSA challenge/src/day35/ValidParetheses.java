1eba98a6cf96ac38264eb08356c55c067cb24cad
Pravin T Kale <pravinpk2002@gmail.com>
2024-12-10 11:52:12 +0530

    day 35 dsa solve balanced paranthesis problem on leet

--- DSA challenge/src/day35/ValidParetheses.java
+++ DSA challenge/src/day35/ValidParetheses.java
@@ -6,16 +6,6 @@ public class ValidParetheses {
 
     private Stack<Character> stack = new Stack<>();
 
-//    for (int i = 0; i < s.length(); i++) {
-//
-//        if(s.charAt(i) == '(' ){
-//            stack.push(s.charAt(i));
-//        }
-//        else if(stack.peek() == '(' && s.charAt(i) == ')')
-//            stack.pop();
-//        else
-//            return false;
-//    }
     public boolean isValid(String s) {
         String strOpen = "({[";
 
