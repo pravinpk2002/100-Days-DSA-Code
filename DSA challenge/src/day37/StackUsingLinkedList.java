40db3dbccb44c847d223edba9f5a1ed7c93b47d2
Pravin T Kale <pravinpk2002@gmail.com>
2024-12-12 09:53:13 +0530

    day 37 dsa implemented stack using linked list

---
+++ DSA challenge/src/day37/StackUsingLinkedList.java
@@ -0,0 +1,44 @@
+package day37;
+
+public class StackUsingLinkedList {
+// solution of GFG
+//    class StackNode {
+//        int data;
+//        StackNode next;
+//        StackNode(int a) {
+//            data = a;
+//            next = null;
+//        }
+//    }
+//    StackNode top;
+//
+//    public MyStack(){
+//        top = null;
+//    }
+//
+//    // Function to push an integer into the stack.
+//    void push(int a) {
+//
+//        StackNode newNode = new StackNode(a);
+//        if (top == null){
+//            top = newNode;
+//            return;
+//        }
+//
+//        newNode.next = top;
+//        top = newNode;
+//
+//    }
+//
+//    // Function to remove an item from top of the stack.
+//    int pop() {
+//        if (top == null)
+//            return -1;
+//
+//        int data = top.data;
+//
+//        top = top.next;
+//
+//        return data;
+//    }
+}
