038f0d503b37d79a8fb610f3e3a9ab968ac30816
Pravin T Kale <pravinpk2002@gmail.com>
2025-01-02 09:00:50 +0530

    day 58 dsa solve sma etree on leetcode

---
+++ DSA challenge/src/day58/Solution.java
@@ -0,0 +1,45 @@
+package day58;
+
+public class Solution {
+
+    public class TreeNode {
+        int val;
+        TreeNode left;
+        TreeNode right;
+        TreeNode() {}
+        TreeNode(int val) { this.val = val; }
+        TreeNode(int val, TreeNode left, TreeNode right) {
+            this.val = val;
+            this.left = left;
+            this.right = right;
+        }
+    }
+
+
+
+    //100. Same Tree
+
+
+    public boolean isSameTree(TreeNode p, TreeNode q) {
+
+        if (p == null && q == null)
+            return true;
+
+        if (p == null || q == null)
+            return false;
+
+        if (p.val != q.val)
+            return false;
+
+        boolean left = isSameTree(p.left,q.left);
+
+        boolean right = isSameTree(p.right,q.right);
+
+        return  left  && right;
+
+    }
+
+    public static void main(String[] args) {
+
+    }
+}
