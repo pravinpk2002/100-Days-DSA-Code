7948cb8aa6114cba9cf3709c4de4e60d8361a465
Pravin T Kale <pravinpk2002@gmail.com>
2025-01-08 09:25:20 +0530

    day 64 dsa solve sort list problem on leetcode

---
+++ DSA challenge/src/day64/Solution.java
@@ -0,0 +1,56 @@
+package day64;
+
+public class Solution {
+//148. Sort List
+    class ListNode {
+        int val;
+        ListNode next;
+
+        ListNode() {}
+        ListNode(int val) { this.val = val; }
+        ListNode(int val, ListNode next) { this.val = val; this.next = next; }
+    }
+
+
+
+    public ListNode sortList(ListNode head) {
+
+        if (head == null || head.next == null)
+            return head;
+
+        ListNode mid = middleNode(head);
+        ListNode left = sortList(head);
+        ListNode right = sortList(mid);
+        return mergeTwoLists(left, right);
+
+    }
+
+    public ListNode middleNode(ListNode head) {
+        ListNode slow = head, fast = head, mid;
+        while (fast.next != null && fast.next.next != null) {
+            slow = slow.next;
+            fast = fast.next.next;
+        }
+        mid = slow.next;
+        slow.next = null;
+        return mid;
+    }
+
+    public ListNode mergeTwoLists(ListNode l1, ListNode l2) {
+        ListNode dummyHead = new ListNode(-1), current = dummyHead;
+        while (l1 != null && l2 != null) {
+            if (l1.val <= l2.val) {
+                current.next = l1;
+                l1 = l1.next;
+            } else {
+                current.next = l2;
+                l2 = l2.next;
+            }
+            current = current.next;
+        }
+        current.next = l1 != null ? l1 : l2;
+        return dummyHead.next;
+    }
+
+
+}
