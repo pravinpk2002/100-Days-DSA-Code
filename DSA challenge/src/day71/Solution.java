75fc99c9e4b5f68726e11d0ccdc548bf3b11f661
Pravin T Kale <pravinpk2002@gmail.com>
2025-01-15 10:00:39 +0530

    day 71 dsa solve decode ways proble on leetcode

---
+++ DSA challenge/src/day71/Solution.java
@@ -0,0 +1,24 @@
+package day71;
+
+public class Solution {
+
+    public int numDecodings(String s) {
+
+        int[] table = new int[s.length()+1];
+        table[0] = 1;
+        table[1] = s.charAt(0) == '0' ? 0 : 1;
+
+        for (int i = 2; i < table.length; i++) {
+            int first = Integer.valueOf(s.substring(i-1,i));
+            int second = Integer.valueOf(s.substring(i-2,i));
+
+            if (first >= 1 && first <= 9)
+                table[i] = table[i-1];
+
+            if (second >=10 && second <= 26)
+                table[i] += table[i-2];
+        }
+
+        return table[table.length-1];
+    }
+}
