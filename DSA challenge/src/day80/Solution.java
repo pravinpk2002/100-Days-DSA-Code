9924da1b9e1ded3582ebca0ed053902c380f5043
Pravin T Kale <pravinpk2002@gmail.com>
2025-01-24 09:36:50 +0530

    day 80 dsa solve climbing stairs problem on leetcode

--- DSA challenge/src/day80/Solution.java
+++ DSA challenge/src/day80/Solution.java
@@ -1,2 +1,19 @@
-package day80;public class Solution {
+package day80;
+
+public class Solution {
+
+//    70. Climbing Stairs
+    public int climbStairs(int n) {
+        if (n <= 1) return 1;
+
+        int[] dp = new int[n + 1];
+        dp[0] = 1;
+        dp[1] = 1;
+
+        for (int i = 2; i <= n; i++) {
+            dp[i] = dp[i - 1] + dp[i - 2];
+        }
+
+        return dp[n];
+    }
 }
