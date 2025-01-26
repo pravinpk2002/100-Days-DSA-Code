57673157589f56cc6bf80286afc18aa74dad9219
Pravin T Kale <pravinpk2002@gmail.com>
2025-01-26 10:25:51 +0530

    day 82 dsa solve partition equal subset sum problem on leetcode

---
+++ DSA challenge/src/day82/Solution.java
@@ -0,0 +1,28 @@
+package day82;
+
+public class Solution {
+
+    public boolean canPartition(int[] nums) {
+        int totalSum = 0;
+        for (int num : nums) {
+            totalSum += num;
+        }
+
+
+        if (totalSum % 2 != 0) {
+            return false;
+        }
+
+        int target = totalSum / 2;
+        boolean[] dp = new boolean[target + 1];
+        dp[0] = true;
+
+        for (int num : nums) {
+            for (int j = target; j >= num; j--) {
+                dp[j] = dp[j] || dp[j - num];
+            }
+        }
+
+        return dp[target];
+    }
+}
