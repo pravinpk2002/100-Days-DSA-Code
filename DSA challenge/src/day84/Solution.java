4e1e6678864fb65a6e0aaddc4f0ebd64ab948cc8
Pravin T Kale <pravinpk2002@gmail.com>
2025-01-28 19:20:34 +0530

    day 84 dsa solve subarray sum equals k on leetcode

---
+++ DSA challenge/src/day84/Solution.java
@@ -0,0 +1,23 @@
+package day84;
+
+import java.util.HashMap;
+
+public class Solution {
+
+//560. Subarray Sum Equals K
+    public int subarraySum(int[] nums, int k) {
+        int count = 0, sum = 0;
+        HashMap<Integer, Integer> map = new HashMap<>();
+        map.put(0, 1);
+
+        for (int num : nums) {
+            sum += num;
+            if (map.containsKey(sum - k)) {
+                count += map.get(sum - k);
+            }
+            map.put(sum, map.getOrDefault(sum, 0) + 1);
+        }
+
+        return count;
+    }
+}
